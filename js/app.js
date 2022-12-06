c00126b5fb64b7d3410b9d606bb9bef04579abcc
ErikUTA <erik4tohj@gmail.com>
2022-12-05 18:43:03 -0600

    feat: added service worker

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-// function ScrollDown(number) {
-//     window.scroll(0, number);
-// }
-
-// const person = {name:"john", lastname:"Doe", age:50}
-// const coppiedPerson = {...person}
-// coppiedPerson = "Juan"
-// console.log(person)
-
-// console.log('object');
+if(navigator.serviceWorker){
+    navigator.serviceWorker.register('../sw.js')
+}
--- sw.js
+++ sw.js
@@ -1,11 +1,40 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/sw.js').then(function (registration) {
-      // Si es exitoso
-      console.log('SW registrado correctamente');
-    }, function (err) {
-      // Si falla
-      console.log('SW fallo', err);
-    });
-  });
-}
+self.addEventListener('install', e => {
+  caches.open('cache-v1')
+      .then( cache => cache.addAll ([
+          'index.html',
+          'https://duracionde.com/storage/images/5f8bb9f954780.jpg',
+          'https://es.digitaltrends.com/wp-content/uploads/2021/08/best-horror-games-little-nightmares.jpg?p=1',
+          'https://pressover.news/wp-content/uploads/2021/10/4K-Pyramid-Head-Wallpaper-scaled.jpg"',
+          'https://www.youtube.com/embed/mrvIaAYZYVw',
+          'https://www.youtube.com/embed/dW6tGA3OUXA',
+          'https://www.youtube.com/embed/EhLs-x8xqBk',
+          'css/index.css',
+          'js/app.js',
+          'js/main.js,',
+          'sw.js'
+      ]));
+      e.waitUntil(cacheProm);
+});
+
+self.addEventListener('fetch', e =>{
+  //cache with network fallback
+  const respuesta = caches.match( e.request )
+      .then ( res => {
+          if ( res ) return res;
+          //no existe el archivo
+          //tengo que ir a la web
+          console.log('No existe', e.request.url);
+          return fetch( e.request ).then ( newResp => {
+              caches.open('cache-v1')
+                  .then( cache => {
+                      cache.put( e.request, newResp);
+                  }
+
+                  )
+              return newResp.clone;
+          });
+      });
+      e.respondWith(respuesta);
+  //only cache
+  //e.respondWith( caches.match(e.request));
+});
